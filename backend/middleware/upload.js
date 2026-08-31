0b04275f36216eb22810315eda60e8280f11d829
sidra <sidrasarwarawan10@gmail.com>
2026-09-01 00:31:17 +0500

    i add it

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -3,8 +3,8 @@ const path = require("path");
 
 const upload = multer({
   dest: path.join(__dirname, "../uploads/"),
-  limits: {
-    fileSize: 5 * 1024 * 1024,
+  limits: { 
+    fileSize: 5 * 1024 * 1024 
   },
   fileFilter: function (req, file, cb) {
     const allowedTypes = ["image/png", "image/gif", "image/jpeg"];
